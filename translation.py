fcd2cddf59051368d0514fe73b8610b716ed3596
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-10 10:49:53 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -34,17 +34,37 @@ Telegram ID: <code>{}</code>
 Plan name : Free User
 Expires on : 31/12/2020
 
-Support Group : @Huduma
+Support Group : @Hudumaclass Translation(object):
+
+START_TEXT = """Bismillah,
+
+Habari yako Mimi ni HamisBit!
+
+nina Husika Na Elimu ya Dini ya kiislam
+
+gusa hapa /help kupata Maelekezo zaidi ..
+
+
 © @Twuwbaa"""
-    HELP_USER = """There are multiple things I can do:
+    HELP_USER = """There are multiple things I can do
 1. Send url (Link|New Name with Extension).
 2. Send Custom Thumbnail (Optional).
 3. Select the button.
    SVideo - Give File as video with Screenshots
    DFile  - Give File with Screenshots
    Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
-   
+   DFile  - Give File without Screenshotclass Translation(object):
+
+START_TEXT = """Bismillah
+
+Habari yako Mimi ni HamisBit!
+
+nina Husika Na Elimu ya Dini ya kiislam
+
+gusa hapa /help kupata Maelekezo zaidi ..
+
+
+  
 <b>👉 Create own Clone Bot : 👉</b> <a href='https://youtu.be/QkAkSLBgoYw'>Deploy</a>
 
 --------
@@ -53,7 +73,7 @@ Send /me to know current plan details
 Support Group : @Huduma
 © @Twuwbaa"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
-    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
     AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AnyDLBot"
@@ -62,7 +82,7 @@ Support Group : @Huduma
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
     FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A saved media already exists. Please send /storageinfo to know the current media details."
     USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
-    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
@@ -71,7 +91,7 @@ Support Group : @Huduma
     EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
     EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@Twuwbaa</a>"
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@Twuwbaa</a>
     EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
 You can use /rename command after receiving file to rename it with custom thumbnail support."""
     CANCEL_STR = "Process Cancelled"
@@ -79,4 +99,22 @@ You can use /rename command after receiving file to rename it with custom thumbn
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other usclass Translation(object):
+
+START_TEXT = """Bismillah,
+
+Habari yako Mimi ni HamisBit!
+
+nina Husika Na Elimu ya Dini ya kiislam
+
+gusa hapa /help kupata Maelekezo zaidi ..
+
+class Translation(object):
+
+START_TEXT = """Bismillah,
+
+Habari yako Mimi ni HamisBit!
+
+nina Husika Na Elimu ya Dini ya kiislam
+
+gusa hapa /help kupata Maelekezo zaidi ..
