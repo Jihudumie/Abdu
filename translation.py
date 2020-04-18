3e47713d1e6dc341d4f6845cf5d43ac7f6bdc009
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-18 16:18:28 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -70,45 +70,7 @@ class Translation(object):
 /upload ⬆️   |   /tafuta 🔍 """
     HAMIS_MAJIBU = "nina /hamis jaribu kujifunza."
     ELIMU_TXT = """
-<b>UTANGULIZI</b> 
-
-Sifa njema anazistahiki Allaah, na rehema na amani zimfikie Bwana wetu, 
-Mtume wetu Muhammad, kipenzi chetu na Mtume wa Allaah. 
-Baada ya hayo, jua ndugu yangu Muislamu, Allaah Akurehemu kwamba 
-inatupasa kujifunza masuala manne: 
-
-<b>1) La Kwanza:</b> Ni Elimu. Nayo ni elimu ya 
-<i>1) Kumjua Allaah Mtukufu. 
-2) Kumjua Mtume Wake (Swalla Allaahu ´alayhi wa sallam). 
-3) Kuijua dini ya Kiislamu,</i>
-
- kwa sababu ilivyo ni kwamba haifai kumuabudu Allaah bila 
-ya elimu. Na anayefanya hivyo, basi mwisho wake ni upotofu, na kwa 
-hakika atakuwa amejifananisha na Wakristo katika hilo. 
-
-<b>2) La Pili:</b> Kutekeleza. Anayejua na asitekeleza, atakuwa amejifananaisha na 
-Wayahudi, kwa sababu wao walijua na hawakutekeleza. Miongoni mwa 
-mbinu za Shetani ni kwamba anafanya elimu ichukiwe akimlaghai 
-mwanadamu kuwa, kwa sababu ya kutojua kwake, basi wakati huo 
-atasamehewa mbele ya Allaah. Hakujua kwamba aliyepata uwezekano wa 
-kusoma na asifanye hivyo, basi imepatikana hoja juu yake. Hii ni mbinu 
-ya watu wa (Mtume) Nuuh kama alivyo waeleza Allaah katika Qur-aan:
-”Waliweka vidole vyao katika masikio yao na kujifunika nguo zao”. 
-<b>[Nuuh 07 : 07].</b> Walifanya hivyo ili isije ikasimama hoja dhidi yao. 
-
-<b>3) La Tatu:</b> Kuihubiri (Kuilingania) elimu hiyo. Kwa sababu wanazuoni na 
-wahubiri (walinganiaji) ndio warithi wa Mitume. Allaah Mtukufu 
-Amewalaani Wana wa Israaiyl kwa sababu ”Walikuwa hawakatazani 
-maovu waliyoyafanya. Kwa yakini ni vibaya sana hivyo walivyokuwa 
-wakifanya”. <b>[al-Maa´idah</b>
-
- 05 : 79]. Elimu na kuhubiri ni Faradhi ya 
-kutoshana. Iwapo wataitekeleza Faradhi hiyo watu ambao wanatosha, 
-hakuna yeyote atakayepata dhambi. Iwapo watu wote wataacha 
-kuitekeleza, basi watapata dhambi wote.
-
-<b>4) La Nne:</b> Ni kuwa na uvumilivu juu ya maudhi katika kujifunza elimu 
-hiyo, kuitekeleza na kuihubiri.
+<b><a href='https://telegra.ph/UTANGULIZI-04-18'>UTANGULIZI</a></b> 
 
 
 
