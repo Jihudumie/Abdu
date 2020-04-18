74bf6004ef01062d295adc0faa8146db3e9e9e3c
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-18 10:57:22 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,4 +1,79 @@
 class Translation(object):
     START_TEXT = "Hello,"
-    
+    HELP_USER = """ /histolia Gusa Command Unayo itaka
+
+In Shaa Allaah Utatumiwa Kilichopo.
+
+/quranswahili 
+Qur'an Audio Lugha Swahili
+
+/tafsir
+Tafsir ya Qur'an
+👇
+/tawhiyd
+👇
+/masomomhimu
+
+---------------------------------
+/nassor_bachu |  /mselemu
+
+/mazoezi  |   /kuogelea
+
+/browser  |   /ilunga
+
+/ibrahim_rogo  |   /stooapk
+
+/fakenumber  |   /update
+
+/mashartiyashahaad
+
+/yalpstore  |   /samiri
+
+/tahadhari  |   /uchumi
+
+/tabianjema  |   /email
+
+/videozadowla2 |    /manhaj
+
+/videozadawla3  |  /aqiydah
+
+/nguzozaswalah   |   /fiqh
+
+/lugha   |   /udhu
+
+/videozadawla1   |   /swalah
+
+/bomulasimu   |   /elimu
+
+/fadhilaislaam   |   /dua
+
+/jihadpdf   |   /ruqya
+
+/masomomhimu
+
+/stker    |     /tawhiyd
+
+/nassor_bacho
+
+/vpn    |    /jihadi 💣
+
+/aqiydtw  |   /abismayl
+
+/dunia  |  /afya  |  /rogo
+
+/daiya  |   /tafsir  |   /fathu
+
+/quran   |  /quranswahili 📖
+
+/sira  |   /swahili
+
+/kujitolea  |   /dola 🔑
+
+/audio  |   /mauti 
+
+/ramadhan  |   /mawaidha
+
+/bm  |   /pdf | /histolia
+
+/upload ⬆️   |   /tafuta 🔍 """
     HAMIS_MAJIBU = "nina /hamis jaribu kujifunza."
