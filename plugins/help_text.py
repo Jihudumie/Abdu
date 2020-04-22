1861fca16230d21085d7efff8b209d03d92e355c
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-22 13:43:39 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -62,7 +62,7 @@ disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["history"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["history", "sira"]))
 async def history_islamic(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/history")
