be6df2fc1a78284a806900dbeb70d60d985468af
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-22 16:50:47 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,160 +4,96 @@ class Translation(object):
 <b>List ya Command Zangu Zoote
 Gusa Command Unayo itaka</b>
       👇👇👇
-
 <b><u>Qur'ani Tukufu</u></b>
 1. /quranswahili   2. /tafsir
-
 3. /Juzuu   4. /quran  
-
  
-
 <b><u>Masheykhe Wa Jihaad</u></b>
 1. /ilunga  2. /mselemu
-
 3. /nassor_bachu   
-
 4. /samiri  5./abismayl
-
 6. /rogo 7. /ibrahim_rogo
-
-
-
 <b><u>Teknolojia</u></b>
 1. /email   3. /browser
-
 3. /fakenumber  4. /update
-
 5. /yalpstore  6. /tahadhari
-
 7. /stker  8. /stooapk
-
 9. /vpn  10. /upload ⬆️  
-
 11. /tafuta 🔍   
 
 
 <b><u>Islamic Start</u></b>
 1. /videozadowla2 
-
 2. /videozadawla3 
-
 3. /videozadawla1
-
 4. /kuogelea  5. /dola 🔑
-
-6. /mazoezi   7. /bomulasimu
-
+6. /mazoezi   7. /audio
 8. /jihadpdf  9. /jihadi 💣  
-
 10. /bm   11. /kujitolea 
-
-12. /audio
-
+12. /bomulasimu
 
 
 <b><u>I Love Islam</u></b>
 1. /elimu  2. /ruqya
-
-3. /masomomhimu  4. /udhu
-
-5. /mashartiyashahaad
-
+3. /masomomhimu  
+4. /mashartiyashahaad
+5. /fadhilaislaam 
 6. /lugha   7. /history
-
-8. /fadhilaislaam  9. /aqiydtw
-
-10. /dunia  11. /afya
-
-12. /daiya   13. /tafsir 
-
-14. /sira   15. /swahili
-
-16. /fathu    17. /mauti 
-
-18. /ramadhan  19. /mawaidha
-
-20. /pdf
+8. /mawaidha 9. /pdf 
+10. /aqiydtw 11. /udhu
+12. /dunia  13. /afya
+14. /daiya   15. /tafsir 
+16. /sira   17. /swahili
+18. /fathu    19. /mauti 
+120. /ramadhan  
 """
     HAMIS_MAJIBU = "https://telegra.ph/i-love-islam-01-24"
     JUZUUZOTE = "https://telegra.ph/Juzuu-ya-1-Hadi-Ya-30-01-22 "    
-    ELIMU_TXT = """<b><i>Kutafuta Elimu Ni Wajibu</i></b>
+    ELIMU_TXT = """<i><b>Kutafuta Elimu Ni Wajibu/b></i>
 
-
-
-
-
-<b><a href='https://telegra.ph/UTANGULIZI-04-18'>UTANGULIZI</a></b>
+<b><a href='https://telegr a.ph/UTANGULIZI-04-18'>UTANGULIZI</a></b>
 
 <pre>Mtume ﷺ amesema(Yeyote yule anayekamata njia kutafuta elimu, Allaah Atamrahisishia njia ya peponi).</pre>
 
-
 ❖ <a href='https://telegra.ph/Aqiydah-06-21'>🅰QIYDAH</a>.  ❖ <a href='https://telegra.ph/Swahiyh-Fiqhis-Sunnah-01-27'>FIQH</a>.
-
-❖ <a href='https://telegra.ph/Manhaj-06-18'>ⓂANHAJ</a>.  ❖ <a href='https://telegra.ph/Tabia-Njema-06-09-2'>TABIA NJEMA</a></a>.  
-
+❖ <a href='https://telegra.ph/Manhaj-06-18'>ⓂANHAJ</a>. ❖ <a href='https://telegra.ph/Jamii-04-21'>Jamii</a>   
+❖ <a href='https://telegra.ph/Tabia-Njema-06-09-2'>TABIA NJEMA</a></a>.
 ❖ <a 
 href='https://telegra.ph/Ramadhaan-Swawm-01-27'>RAMADHAN</a>.  ❖ <a href='https://telegra.ph/Janaaiz-01-27'>Janaaiz</a>.
-
 ❖ <a
 href='https://telegra.ph/Nguzo-Za-Swalaah-Vitendo-Vya-Waajib-Na-Sunnah-Katika-Swalaah-01-27'>NGUZO ZA SWALAH</a>.
-
 ❖ <a
 href='https://telegra.ph/Hajj-01-27'>Hajj</a>.   ❖ <a href='https://telegra.ph/Swalah-01-27'>Swalah</a>.
-
 ❖ <a href='https://telegra.ph/MASHARTI-YA-KUKUBALIWA-SHAHADA-LA-ILAHA-ILA-LLAH-09-12'>Ⓜasharti Ya Shahaad Na Mambo Yake</a>.
-
 ❖ <a href='https://wanachuoni.com/sidebar_menu_list/aqiydah/'> Category: ´Aqiydah</a>.
 ▬▬▬▬▬▬▬▬▬▬
-
-
 📖 <a 
 href='https://telegra.ph/Swalah-01-27'>SWALAH 🕯</a>
-
 📖 <a href='https://telegra.ph/Sifa-Ya-Swalah-03-08'>Sifa Ya Swalah Ya Mtume (Swalla Allaahu 'Alayhi Wa Aalihi Wa Sallam)</a>
-
 📖 <a href='https://telegra.ph/Swalaah--Kuwajibika-Na-Fadhila-Zake-01-27-2'>Swalaah – Kuwajibika Na Fadhila Zake</a>
 ▬▬▬▬▬▬▬▬▬▬
-
    ❖ <a
 href='https://telegra.ph/Tawhiyd-01-24'>TAWHIYD 🎯</a>
-
 <a href='https://telegra.ph/Umuhimu-Na-Fadhila-Za-Tawhiyd-01-30'>Umuhimu Na Fadhila Za Tawhiyd</a>
-
 <a href='https://telegra.ph/Majina-Ya-Allaah-Na-Sifa-Zake-AL-HAKAM-01-31'>Majina Ya Allaah Na Sifa Zake: AL-HAKAM</a>
 ▬▬▬▬▬▬▬▬▬▬
-
 ❖ <a href='https://telegra.ph/Duaa-Na-Adkhaar-01-24'>Dua Na Adkhaar 👅</a>
-
 <a href='https://telegra.ph/Duaa-Za-Nabiy-%D8%B5%D9%84%D9%89-%D8%A7%D9%84%D9%84%D9%87-%D8%B9%D9%84%D9%8A%D9%87-%D9%88%D8%A2%D9%84%D9%87-%D9%88%D8%B3%D9%84%D9%85-01-27'>Du'aa Za Nabiy (صلى الله عليه وآله وسلم)</a>
-
 <a href='https://telegra.ph/Ruqyah-Kinga-Na-Tiba-Katika-Shariyah-01-27'>Ruqyah: Kinga Na Tiba Katika Shariy'ah</a>
-
 <a href='https://telegra.ph/Duaa-Za-Ruqyah-Kinga-Faida-Na-Sharh-Zake-01-27'>Du’aa Za Ruqyah (Kinga): Faida Na Sharh Zake</a>
 ▬▬▬▬▬▬▬▬▬▬
        
-
 <b>RIZKI 🌽 NA UCHUMI ⛵️</b>
-
 ✿ <i><a href='https://telegra.ph/UTANGULIZI-09-12'>UTANGULIZI</a></i>
-
 ✿ <i><a href='https://telegra.ph/HIMIZO-LA-UISLAM-KUHUSU-UTAFUTAJI-RIZKI-NA-UCHUMI-09-12'>HIMIZO LA UISLAM KUHUSU UTAFUTAJI RIZKI NA UCHUMI</a></i>
-
 ✿ <i><a href='https://telegra.ph/Kazi-Katika-Uislam-09-12'>Kazi Katika Uislam</a></i>
-
 ✿ <i><a href='https://telegra.ph/HADHI-YA-KAZI-09-12'>HADHI YA KAZI</a></i>
-
 ✿ <i><a href='https://telegra.ph/KAZI-ZILIZOHARAMISHWA-09-12'>KAZI ZILIZOHARAMISHWA</a></i>
-
 ✿ <i><a href='https://telegra.ph/SIFA-NZURI-ZA-MFANYAKAZI-MUISLAM-09-12'>SIFA NZURI ZA MFANYAKAZI MUISLAM</a></i>
-
-✿ <i><a href='https://telegra.ph/SIFA-NZURI-ZA-MWAJIRI-MUISLAM-09-12'>SIFA NZURI ZA MWAJIRI MUISLAM</a></i>
-▬▬▬▬▬▬▬▬▬▬
-
+✿ <i><a href='https://telegra.ph/SIFA-NZURI-ZA-MWAJIRI-MUISLAM-09-12'>SIFA NZURI ZA MWAJIRI MUISLAM</a>/i>
+▬▬▬▬▬▬▬▬▬▬ 
+  
 ❖ <b><a href='https://telegra.ph/Zakaah-Swadaqah-01-27'>Zakaah-Swadaqah</a></b>
-
  <a href='https://telegra.ph/Kutoa-Zakaah-01-31'>Kutoa Zakaah</a>.
-
-
 """
     HISTORY_ISLAMIC = "Gusa hapo👇\n\n<a href='https://islamichistory.org/'>Islamic History</a>"
