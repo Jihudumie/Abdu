3288e98f010873aee197aa6a58f6cef5710f1ca2
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-23 20:05:21 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -16,9 +16,9 @@ class Config(object):
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 1572864000
-    FREE_USER_MAX_FILE_SIZE = 50000000
+    MAX_FILE_SIZE = 1
+    TG_MAX_FILE_SIZE = 1
+    FREE_USER_MAX_FILE_SIZE = 1
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
@@ -29,8 +29,8 @@ class Config(object):
     # https://t.me/hevcbay/951
     OUO_IO_API_KEY = ""
     # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
+    MAX_MESSAGE_LENGTH = 1
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
+    PROCESS_MAX_TIMEOUT = 1
     # watermark file
     DEF_WATER_MARK_FILE = ""
